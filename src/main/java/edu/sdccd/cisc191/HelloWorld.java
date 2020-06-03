593e0227555d6911982e871a4d9f339cc88446c7
Andrew Huang <andrew@ax2d.com>
2020-06-03 15:23:20 -0700

    Added javadoc annotation examples

--- src/main/java/edu/sdccd/cisc191/HelloWorld.java
+++ src/main/java/edu/sdccd/cisc191/HelloWorld.java
@@ -1,5 +1,8 @@
 package edu.sdccd.cisc191;
 
+/**
+ * A class to store hello world! strings
+ */
 public class HelloWorld {
     private String helloWorld;
 
@@ -7,10 +10,18 @@ public class HelloWorld {
         helloWorld = "hello world!";
     }
 
+    /**
+     * Getter for helloWorld object variable
+     * @return helloWorld
+     */
     public String getHelloWorld() {
         return helloWorld;
     }
 
+    /**
+     * Setter for helloWorld object variable
+     * @param helloWorld a hello world! string
+     */
     public void setHelloWorld(String helloWorld) {
         this.helloWorld = helloWorld;
     }
