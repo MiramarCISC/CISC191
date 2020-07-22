bcb634afc1d259e7bfac9e664e67e814482738b1
Andrew Huang <andrew@ax2d.com>
2020-07-22 15:42:35 -0700

    Added DatabaseLab module which demonstrates Spring JPA with embedded H2 database per this link: https://spring.io/guides/gs/accessing-data-jpa/

---
+++ DatabaseLab/src/main/java/edu/sdccd/cisc191/CustomerRepository.java
@@ -0,0 +1,12 @@
+package edu.sdccd.cisc191;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+    List<Customer> findByLastName(String lastName);
+
+    Customer findById(long id);
+}
