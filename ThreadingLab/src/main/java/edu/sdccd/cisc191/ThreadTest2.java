f5626179e7ff63115a73be07ee57c885833bd8ce
Andrew Huang <ahuang@sdccd.edu>
2020-10-09 11:59:41 -0700

    Revert "Added broken synchronized block to ThreadTest2.java"
    This reverts commit c478a472

--- ThreadingLab/src/main/java/edu/sdccd/cisc191/ThreadTest2.java
+++ ThreadingLab/src/main/java/edu/sdccd/cisc191/ThreadTest2.java
@@ -26,11 +26,9 @@ public class ThreadTest2 {
      * Adds x to total.  This method is synchronized so that it can be safely used by
      * several different threads.
      */
-    private static void addToTotal(int x) {
-        synchronized (this) {
-            total = total + x;
-            System.out.println(total + " primes found so far.");
-        }
+    synchronized private static void addToTotal(int x) {
+        total = total + x;
+        System.out.println(total + " primes found so far.");
     }
 
     /**
